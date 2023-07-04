8d3d4f937e67b7a405254fac1510d34964231e87
rajRishi22 <98014028+rajRishi22@users.noreply.github.com>
2023-07-05 01:54:05 +0530

    Create Subarraysumdivbyk.cpp
    I still need to better understand the advanced prefixsum approach used here.Did it by taking a hint.

---
+++ Arrays/Medium/Subarraysumdivbyk.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> sum(k,0);
+        sum[0]=1;
+        int count=0;
+        int currsum=0;
+        for(int i=0;i<n;i++){
+            currsum=(currsum+nums[i]%k+k)%k;
+            count=count+sum[currsum];
+            sum[currsum]++;
+
+
+        }
+        return count;
+
+    }
+};
